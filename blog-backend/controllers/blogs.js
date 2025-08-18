6d9330d77a837916fd8eebfd0d163de8494f55f3
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2025-08-17 21:12:23 -0400

    test cases and update blogs endpoint

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/models.js');
+const User = require('../models/user.js');
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({});
@@ -7,12 +8,29 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body);
+    const body = request.body;
 
-    if (!blog.url || !blog.title || !blog.likes) {
+    const user = await User.findById(body.userId);
+    if (!user) {
+        return response.status(400).json({ error: 'user not found' });
+    }
+
+    if (!body.url || !body.title || !body.likes) {
         return response.status(400).json({ error: 'url or title or likes is missing' });
     }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    });
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(blog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
 });
 
